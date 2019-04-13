bb0ad2888679c4bc59e5c41d7df49a03bf9061ee
Ryan Bezdicek <rbezdicek@cray.com>
2019-04-12 19:40:55 -0500

    Debug autotag

--- runPostPublish.sh
+++ runPostPublish.sh
@@ -3,6 +3,8 @@
 VERSION="$(cat ./version).$BUILD_NUMBER"
 TAG="v$VERSION"
 
+echo "BRANCH: ${GIT_BRANCH}"
+
 if [[ "$GIT_BRANCH" == 'origin/master' ]]; then
     git tag $TAG && git push origin --tags
 fi
