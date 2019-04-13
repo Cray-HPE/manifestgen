199f443154d26854e8322482c0e6526221b00679
Ryan Bezdicek <rbezdicek@cray.com>
2019-04-12 19:49:19 -0500

    Also check for master instead of just origin/master

--- runPostPublish.sh
+++ runPostPublish.sh
@@ -5,6 +5,6 @@ TAG="v$VERSION"
 
 echo "BRANCH: ${GIT_BRANCH}"
 
-if [[ "$GIT_BRANCH" == 'origin/master' ]]; then
+if [[ "$GIT_BRANCH" == 'origin/master' ||  "$GIT_BRANCH" == 'master' ]]; then
     git tag $TAG && git push origin --tags
 fi
